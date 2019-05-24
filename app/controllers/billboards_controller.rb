41ae285bea6ae583310eebf45e68642770138591
Kristoffer Keene <toffer2001@yahoo.com>
2019-05-23 20:20:03 -0600

    updating billboard controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,9 +1,17 @@
 class BillboardsController < ApplicationController
+  
+  before_action: set_billbaord, only: [:show, :edit, :update, :destroy]
+
   def index
+    @billboards = Billboard.all
 
   end
 
   def show
+    # before_action
+  end
+
+  def new
 
   end
 
@@ -11,9 +19,26 @@ class BillboardsController < ApplicationController
 
   end
 
-  def new
-
+  def edit
+    # before_action
   end
 
+  def update
+    # before_action
+
+    # neecd to include if/else
   
+  end
+
+  def destroy
+    # before_action
+  
+  end
+
+  private
+
+  def set_billboard
+    @billboard = Billboard.find(params[:id])
+  end
+
 end
